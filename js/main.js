a7105ec42920188b13f461345345638d0734170e
Constantine <qostyab@gmail.com>
2016-02-22 19:16:40 +0300

    + main.js, animations, actions, fixes and improvements

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function ($) {
+    'use strict';
+
+    $('.js-show-win').click(
+        function () {
+            $('.b-modal_gift_success')
+                .addClass('_active');
+
+            // Здесь мог бы быть ajax-запрос для отправки подарка:
+
+            //$.post('/api/send-gift/', {id: 42}).then(
+            //    function (response) {
+            //        if (response.is_coincidence) {
+            //            $('.b-modal_gift_success')
+            //                .addClass('_active');
+            //        }
+            //
+            //        alert('Поздравляю, успешный запрос!');
+            //    }
+            //);
+
+            return false;
+        }
+    );
+
+    $('.js-hide-win').click(
+        function () {
+            $('.b-modal_gift_success')
+                .removeClass('_active');
+
+            return false;
+        }
+    );
+
+}(jQuery));
